2ad51f35e3ce43ae48949244fc9515624232f22b
Manikandan <82508909+Manikandan2509@users.noreply.github.com>
2022-06-07 08:09:44 +0530

    Add files via upload

---
+++ Perfect square between two numbers.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main(String[] args){
+	int a = 10;
+	int b = 100;
+	for(int i=0;i*i<b;i++)
+	{
+		if(i*i > a && i*i < b){
+		System.out.print(i*i+" ");
+}
+	}
+}
+}
